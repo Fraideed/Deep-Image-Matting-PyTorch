c0911399e14ebb971f7c4ee9d42b824c428f500b
Fraideed <2690632679@qq.com>
2021-03-05 17:41:20 +0800

    coarse2

--- others/segment.py
+++ others/segment.py
@@ -1,30 +1,20 @@
 import os
-from io import BytesIO
 import tarfile
-import tempfile
-from six.moves import urllib
-
-from matplotlib import gridspec
-from matplotlib import pyplot as plt
 import numpy as np
 from PIL import Image
-
 import tensorflow as tf
 import cv2
 
 
 class DeepLabModel(object):
     """Class to load deeplab model and run inference."""
-
     INPUT_TENSOR_NAME = 'ImageTensor:0'
     OUTPUT_TENSOR_NAME = 'SemanticPredictions:0'
     INPUT_SIZE = 513
     FROZEN_GRAPH_NAME = 'frozen_inference_graph'
-
     def __init__(self, tarball_path):
         """Creates and loads pretrained deeplab model."""
         self.graph = tf.Graph()
-
         graph_def = None
         # Extract frozen graph from tar archive.
         tar_file = tarfile.open(tarball_path)
@@ -33,23 +23,17 @@ class DeepLabModel(object):
                 file_handle = tar_file.extractfile(tar_info)
                 graph_def = tf.compat.v1.GraphDef.FromString(file_handle.read())
                 break
-
         tar_file.close()
-
         if graph_def is None:
             raise RuntimeError('Cannot find inference graph in tar archive.')
-
         with self.graph.as_default():
             tf.import_graph_def(graph_def, name='')
-
         self.sess = tf.compat.v1.Session(graph=self.graph)
 
     def run(self, image):
         """Runs inference on a single image.
-
       Args:
         image: A PIL.Image object, raw input image.
-
       Returns:
         resized_image: RGB image resized from original input image.
         seg_map: Segmentation map of `resized_image`.
@@ -64,186 +48,57 @@ class DeepLabModel(object):
         seg_map = batch_seg_map[0]
         return resized_image, seg_map
 
-
 def create_pascal_label_colormap():
     """Creates a label colormap used in PASCAL VOC segmentation benchmark.
-
   Returns:
     A Colormap for visualizing segmentation results.
   """
     colormap = np.zeros((256, 3), dtype=int)
     ind = np.arange(256, dtype=int)
-
     for shift in reversed(range(8)):
         for channel in range(3):
             colormap[:, channel] |= ((ind >> channel) & 1) << shift
         ind >>= 3
-
     return colormap
-
 
 def label_to_color_image(label):
     """Adds color defined by the dataset colormap to the label.
-
   Args:
     label: A 2D array with integer type, storing the segmentation label.
-
   Returns:
     result: A 2D array with floating type. The element of the array
       is the color indexed by the corresponding element in the input label
       to the PASCAL color map.
-
   Raises:
     ValueError: If label is not of rank 2 or its value is larger than color
       map maximum entry.
   """
     if label.ndim != 2:
         raise ValueError('Expect 2-D input label')
-
     colormap = create_pascal_label_colormap()
-
     if np.max(label) >= len(colormap):
         raise ValueError('label value too large.')
-
     return colormap[label]
 
 
-def vis_segmentation(image, seg_map):
-    """Visualizes input image, segmentation map and overlay view."""
-    plt.figure(figsize=(15, 5))
-    grid_spec = gridspec.GridSpec(1, 4, width_ratios=[6, 6, 6, 1])
-
-    plt.subplot(grid_spec[0])
-    plt.imshow(image)
-    plt.axis('off')
-    plt.title('input image')
-
-    plt.subplot(grid_spec[1])
-    seg_image = label_to_color_image(seg_map).astype(np.uint8)
-    seg_image = Image.fromarray(seg_image)
-    seg_image = seg_image.resize(original_im.size)
-    # new_in=Image.fromarray(seg_image)
-    # new_in.save('seg.png')
-    # plt.imshow(seg_image)
-    # plt.axis('off')
-    # plt.title('segmentation map')
-    # gray = Image.open('seg.png').convert('L')
-    # threshold = 5
-    # table = []
-    # for i in range(256):
-    #     if i < threshold:
-    #         table.append(0)
-    #     else:
-    #         table.append(1)
-    #
-    # #  convert to binary image by the table
-    # bim = gray.point(table, '1' )
-    # bim2=bim.resize(original_im.size)
-    # bim2.save('binary.png')
-    # bim2.show()
-    ori=cv2.cvtColor(np.asarray(original_im),cv2.COLOR_RGB2BGR)
-    seg=cv2.cvtColor(np.asarray(seg_image),cv2.COLOR_RGB2BGR)
-    gray=cv2.cvtColor(seg,cv2.COLOR_BGR2GRAY)
-    ret,mask=cv2.threshold(gray,0,255,cv2.THRESH_BINARY)
-    # ret2,mask2=cv2.threshold(gray,0,255,cv2.THRESH_BINARY_INV)
-    # output=image=cv2.add(ori, np.zeros(np.shape(ori), dtype=np.uint8), mask=mask)
-    # output = cv2.bitwise_and(ori,np.zeros(np.shape(ori),dtype=np.uint8), mask=mask)
-    ori[mask == 0] = (255, 255, 255)
-    cv2.imshow('output',ori)
-    cv2.waitKey(0)
-
-
-
-    # print(mask.shape)
-    # print(image.shape)
-    # cv2.imwrite('ori.jpg', image)
-    # ori = cv2.imread('ori.jpg')
-    # cv2.imshow('ori',ori)
-    # output = cv2.add(ori, np.zeros(np.shape(ori), dtype=np.uint8), mask=mask)
-    # cv2.imshow('output', output)
-
-    plt.subplot(grid_spec[2])
-    plt.imshow(image)
-    plt.imshow(seg_image, alpha=0.7)
-    plt.axis('off')
-    plt.title('segmentation overlay')
-
-    unique_labels = np.unique(seg_map)
-    ax = plt.subplot(grid_spec[3])
-    plt.imshow(
-        FULL_COLOR_MAP[unique_labels].astype(np.uint8), interpolation='nearest')
-    ax.yaxis.tick_right()
-    plt.yticks(range(len(unique_labels)), LABEL_NAMES[unique_labels])
-    plt.xticks([], [])
-    ax.tick_params(width=0.0)
-    plt.grid('off')
-    plt.show()
-
-
-LABEL_NAMES = np.asarray([
-    'background', 'aeroplane', 'bicycle', 'bird', 'boat', 'bottle', 'bus',
-    'car', 'cat', 'chair', 'cow', 'diningtable', 'dog', 'horse', 'motorbike',
-    'person', 'pottedplant', 'sheep', 'sofa', 'train', 'tv'
-])
-
-FULL_LABEL_MAP = np.arange(len(LABEL_NAMES)).reshape(len(LABEL_NAMES), 1)
-FULL_COLOR_MAP = label_to_color_image(FULL_LABEL_MAP)
-
-MODEL_NAME = 'mobilenetv2_coco_voctrainaug'  # @param ['mobilenetv2_coco_voctrainaug', 'mobilenetv2_coco_voctrainval', 'xception_coco_voctrainaug', 'xception_coco_voctrainval']
-
-_DOWNLOAD_URL_PREFIX = 'http://download.tensorflow.org/models/'
-_MODEL_URLS = {
-    'mobilenetv2_coco_voctrainaug':
-        'deeplabv3_mnv2_pascal_train_aug_2018_01_29.tar.gz',
-    'mobilenetv2_coco_voctrainval':
-        'deeplabv3_mnv2_pascal_trainval_2018_01_29.tar.gz',
-    'xception_coco_voctrainaug':
-        'deeplabv3_pascal_train_aug_2018_01_04.tar.gz',
-    'xception_coco_voctrainval':
-        'deeplabv3_pascal_trainval_2018_01_04.tar.gz',
-}
-_TARBALL_NAME = 'deeplab_model.tar.gz'
-
-model_dir = tempfile.mkdtemp()
-tf.io.gfile.makedirs(model_dir)
-
-# download_path = os.path.join(model_dir, _TARBALL_NAME)
-# print('downloading model, this might take a while...')
-# urllib.request.urlretrieve(_DOWNLOAD_URL_PREFIX + _MODEL_URLS[MODEL_NAME],
-#                    download_path)
-# print('download completed! loading DeepLab model...')
 download_path = 'deeplabv3_mnv2_pascal_train_aug_2018_01_29.tar.gz'
 MODEL = DeepLabModel(download_path)
 print('model loaded successfully!')
-
-
-SAMPLE_IMAGE = 'image1'  # @param ['image1', 'image2', 'image3']
-IMAGE_URL = ''  # @param {type:"string"}
-
-_SAMPLE_URL = ('https://github.com/tensorflow/models/blob/master/research/'
-               'deeplab/g3doc/img/%s.jpg?raw=true')
-
-# def run_visualization(url):
-#   """Inferences DeepLab model and visualizes result."""
-#   try:
-#     f = urllib.request.urlopen(url)
-#     jpeg_str = f.read()
-#     original_im = Image.open(BytesIO(jpeg_str))
-#   except IOError:
-#     print('Cannot retrieve image. Please check url: ' + url)
-#     return
-#
-#   print('running deeplab on image %s...' % url)
-#   resized_im, seg_map = MODEL.run(original_im)
-#
-#   vis_segmentation(resized_im, seg_map)
-
-
-image_url = IMAGE_URL or _SAMPLE_URL % SAMPLE_IMAGE
-img_path = 'cat.png'
+img_path = 'shoe.png'
 original_im = Image.open(img_path)
 resized_im, seg_map = MODEL.run(original_im)
-vis_segmentation(resized_im, seg_map)
+
+seg_image = label_to_color_image(seg_map).astype(np.uint8)
+seg_image = Image.fromarray(seg_image)
+seg_image = seg_image.resize(original_im.size)
+ori = cv2.cvtColor(np.asarray(original_im), cv2.COLOR_RGB2BGR)
+seg = cv2.cvtColor(np.asarray(seg_image), cv2.COLOR_RGB2BGR)
+gray = cv2.cvtColor(seg, cv2.COLOR_BGR2GRAY)
+ret, mask = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY)
+
+ori[mask == 0] = (255, 255, 255)
+cv2.imshow('output', ori)
+cv2.waitKey(0)
 
 
 
